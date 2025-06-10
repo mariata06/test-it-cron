1e38bb8a6eb1e21c43885c2f93ea802e3ddf5b2f
mariata06 <slinkywizard@mail.ru>
2025-06-10 07:19:04 +0300

    Add some changes

--- source/js/main.js
+++ source/js/main.js
@@ -13,14 +13,6 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    // initModals();
-    // uploadFile();
-    // uploadImageDrop();
-    // const select = new CustomSelect();
-    // select.init();
-    // const form = new Form();
-    // window.form = form;
-    // form.init();
-    // reviewSwiper();
+
   });
 });
